d05bbfd7f4046165b3745d0d1df9406ca9c8c743
Michael Dan Mossinsohn <consistent1@gmail.com>
2013-08-15 21:29:56 +0000

    Some chages... May not make it to production.

--- web.js
+++ web.js
@@ -11,7 +11,19 @@ app.get('/', function(request, response) {
 
 });
 
-var port = process.env.PORT || 5000;
+
+app.get('/play.html', function(request, response) {
+
+  var play = fs.readFileSync('play.html');
+  var playString = play.toString();
+  response.send(playString);
+
+});
+
+
+
+
+var port = process.env.PORT || 8080;//5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
